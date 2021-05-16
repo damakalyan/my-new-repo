603d06a86597c3e698aa39173a852e26c56cb361
damakalyan <damakalyan@gmail.com>
2021-05-16 16:36:04 +0530

    Updated date of birth to person

--- Applications/ContinuousIntegrationAndDeployment.Model/Person.cs
+++ Applications/ContinuousIntegrationAndDeployment.Model/Person.cs
@@ -1,9 +1,13 @@
-﻿namespace ContinuousIntegrationAndDeployment.Model
+﻿using System;
+
+namespace ContinuousIntegrationAndDeployment.Model
 {
     public class Person
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool HasRegistered { get; set; }
     }
 }
